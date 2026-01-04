be8e600a06150b3fdaf8fd9ba329214304c32c9e
fuentescruz940-ar <fuentescruz940@gmail.com>
2026-01-03 19:09:56 -0600

    Pruebas

---
+++ square-shell/src/config/animations.js
@@ -0,0 +1,5 @@
+export const animations = {
+    hoverScale: "transition-transform duration-300 ease-out hover:scale-105",
+    softFade: "opacity-0 translate-y-4 transition-all duration-500 ease-out",
+    parallax: "will-change-transform motion-reduce:transform-none",
+};
---
+++ square-shell/src/config/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    darkBlue: "#0A0F1F",
+    cyan: "#00E5FF",
+    purple: "#9D4EDD",
+    pink: "#FF6EC7",
+    neonGreen: "#39FF14",
+    softWhite: "#F8F8FA",
+    softPurple: "#C084FC",
+};
